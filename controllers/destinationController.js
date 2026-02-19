eff0506d42f8290ff6b161b1c828ab5139fc42ed
roky18 <rokymax66@gmail.com>
2026-02-20 01:02:23 +0600

    [ADDED]: Destinations Pagination

--- controllers/destinationController.js
+++ controllers/destinationController.js
@@ -2,9 +2,8 @@ const { getDB } = require("../config/db");
 const { destinations } = require("../constants/collections");
 const { ObjectId } = require("mongodb");
 
-
 const getDestinations = async (req, res) => {
-  const { city, country, region } = req.query;
+  const { city, country, region, page = 1, limit = 10 } = req.query;
   const db = getDB();
 
   let filters = [];
@@ -27,8 +26,24 @@ const getDestinations = async (req, res) => {
   }
 
   try {
-    const result = await db.collection(destinations).find(query).toArray();
-    res.send(result);
+    //  Pagination calculation
+    const skip = (parseInt(page) - 1) * parseInt(limit);
+    const lim = parseInt(limit);
+    const totalCount = await db.collection(destinations).countDocuments(query);
+    const totalPages = Math.ceil(totalCount / lim);
+    const data = await db
+      .collection(destinations)
+      .find(query)
+      .skip(skip)
+      .limit(lim)
+      .toArray();
+
+    res.send({
+      data,
+      page: parseInt(page),
+      totalPages,
+      totalCount,
+    });
   } catch (err) {
     console.error(err);
     res.status(500).send({ error: "Server error" });
@@ -37,13 +52,15 @@ const getDestinations = async (req, res) => {
 
 const getDestinationById = async (req, res) => {
   try {
-    const { id } = req.params; 
+    const { id } = req.params;
     if (!ObjectId.isValid(id)) {
       return res.status(400).send({ error: "Invalid ID" });
     }
 
     const db = getDB();
-    const destination = await db.collection(destinations).findOne({ _id: new ObjectId(id) });
+    const destination = await db
+      .collection(destinations)
+      .findOne({ _id: new ObjectId(id) });
 
     if (!destination) {
       return res.status(404).send({ error: "Destination not found" });
@@ -56,4 +73,4 @@ const getDestinationById = async (req, res) => {
   }
 };
 
-module.exports = {getDestinations, getDestinationById };
+module.exports = { getDestinations, getDestinationById };
