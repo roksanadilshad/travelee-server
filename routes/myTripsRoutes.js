fa76cdae17dfab349e144995d0c6e8e59d468043
Roksana Dilshad <roksanadilshad@gmail.com>
2026-03-11 11:47:11 +0600

    [FIXED]: Error fixed

--- routes/myTripsRoutes.js
+++ routes/myTripsRoutes.js
@@ -2,11 +2,6 @@ const express = require("express");
 const { getMyTrips, deleteMyTrip, addToMyTrips } = require("../controllers/myTripsController");
 const auth = require("../middlewares/auth")
 const router = express.Router();
-const {
-  getMyTrips,
-  deleteMyTrip,
-  addToMyTrips,
-} = require("../controllers/myTripsController");
 
 const {
   inviteMember,
