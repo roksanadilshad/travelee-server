a47eda48d0e01a447a3c49b137280ad0e242dc3c
Abdul-Kader-Rimon <abdulkaderrimon8@gmail.com>
2026-03-15 14:20:44 +0600

    KAN-57[ADDED]:user profile update functionality

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,14 +1,16 @@
 const express = require('express');
-const { getUser, getSingleUser, createNewUser, ForgotPassword, ResetPassword } = require('../controllers/userController');
+const { getUser, getSingleUser, createNewUser, ForgotPassword, ResetPassword, updateProfile } = require('../controllers/userController');
 
 const router = express.Router();
 
 const auth = require("../middlewares/auth");
+const authMiddleware = require('../middlewares/authMiddleware');
 
 router.get("/", auth, getUser);
 router.post('/email', getSingleUser);
 router.post('/', createNewUser);
 router.post('/forgot-password', ForgotPassword);
 router.post('/reset-password', ResetPassword);
+router.patch('/update-profile', authMiddleware, updateProfile);
 
 module.exports = router;
