2d659c6a2b19b01b146963f6c410b0190e725eaf
Abdul-Kader-Rimon <abdulkaderrimon8@gmail.com>
2026-03-19 22:43:48 +0600

    KAN-58[ADDED]:Create role base dashboard and Admin Panel: User Management System

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -6,7 +6,7 @@ const router = express.Router();
 const auth = require("../middlewares/auth");
 const authMiddleware = require('../middlewares/authMiddleware');
 
-router.get("/", auth, getUser);
+router.get("/", authMiddleware, getUser);
 router.post('/email', getSingleUser);
 router.post('/', createNewUser);
 router.post('/forgot-password', ForgotPassword);
