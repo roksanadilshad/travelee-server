97b51aa7f2d876db90deb05192c13c2377a0af4f
roky18 <rokymax66@gmail.com>
2026-03-31 03:31:55 +0600

    KAN-60[FIXED/ADDED]: Admin Browse & analytics and revenue management system - Roky

--- routes/allTripRoutes.js
+++ routes/allTripRoutes.js
@@ -3,8 +3,7 @@ const { getAllTrips } = require('../controllers/tripController');
 const authMiddleware = require('../middlewares/authMiddleware');
 
 const router = express.Router();
-
-
+ 
 router.get("/", authMiddleware, getAllTrips);
 
 module.exports = router;
